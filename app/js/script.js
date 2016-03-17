eb26ca4d6d5e9205d44a3f394ffa6c7bdb675b81
Your Name <you@example.com>
2016-03-17 21:08:03 +0300

    add basket + menu

--- app/js/script.js
+++ app/js/script.js
@@ -11,6 +11,23 @@
 
 $(document).ready(function(){
 
+	$('.minus').click(function () {
+	        var $input = $(this).parent().find('input');
+	        var count = parseInt($input.val()) - 1;
+	        count = count < 1 ? 0 : count;
+	        $input.val(count);
+	        $input.change();
+	        return false;
+
+	    });
+
+	$('.plus').click(function () {
+	    var $input = $(this).parent().find('input');
+	    $input.val(parseInt($input.val()) + 1);
+	    $input.change();
+	    return false;
+
+	});
 	/* ###### init EasyDropDown style for selects  ######*/
 	/* ###### bower i easydropdown  ######*/
 	/*<select class="dropdown"> add class (dropdown)
